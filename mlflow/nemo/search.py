6e9db2f1715ea14a605b5b9b577cda50428ab019
kqf <q.dev.null@gmail.com>
2025-03-31 07:45:13 +0100

    Add the logic (#5)
    * Add the test workflow
    
    * Add the publish workflow
    
    * Add some logic
    
    * Add some logic
    
    * Make it namespace package

--- mlflow/nemo/search.py
+++ mlflow/nemo/search.py
@@ -1,5 +1,13 @@
 from typing import List
 
+import mlflow
 
-def find_run(run_name: str) -> List[str]:
-    return []
+
+def find_run(experiment_id: str, model_name: str) -> List[str]:
+    runs = mlflow.search_runs(experiment_ids=[experiment_id])
+
+    matching_runs = runs[
+        runs["tags.mlflow.runName"].str.contains(model_name, na=False)
+    ]
+
+    return matching_runs["run_id"].tolist()
---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import os
+
+import pytest
+
+import mlflow
+
+
+@pytest.fixture(scope="function")
+def mlflow_experiment(tmp_path):
+    os.environ["MLFLOW_TRACKING_URI"] = f"file://{tmp_path}"
+    mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
+    experiment_id = mlflow.create_experiment("test-experiment")
+
+    with mlflow.start_run(run_name="test-run-1"):
+        mlflow.log_param("param1", 10)
+
+    with mlflow.start_run(run_name="test-run-2"):
+        mlflow.log_param("param2", 20)
+
+    # Simulate the multiple runs with the same name
+    with mlflow.start_run(run_name="test-run-2"):
+        mlflow.log_param("param2", 23)
+
+    yield experiment_id
+
+    # Cleanup
+    del os.environ["MLFLOW_TRACKING_URI"]
