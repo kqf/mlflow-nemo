7056c1224c70f18065f58111e3d48b718371b8a5
kqf <q.dev.null@gmail.com>
2025-03-31 08:13:57 +0100

    Add the dummy tests

--- mlflow/nemo/search.py
+++ mlflow/nemo/search.py
@@ -3,11 +3,11 @@ from typing import List
 import mlflow
 
 
-def find_run(experiment_id: str, model_name: str) -> List[str]:
+def find_run(experiment_id: str, run_name: str) -> List[str]:
     runs = mlflow.search_runs(experiment_ids=[experiment_id])
 
     matching_runs = runs[
-        runs["tags.mlflow.runName"].str.contains(model_name, na=False)
+        runs["tags.mlflow.runName"].str.contains(run_name, na=False)
     ]
 
     return matching_runs["run_id"].tolist()
