b210c3e125fdccc190913d215d97135655caf76d
kqf <q.dev.null@gmail.com>
2025-03-31 08:19:32 +0100

    Fix the tests (#6)
    * Add the test workflow
    
    * Add the publish workflow
    
    * Add some logic
    
    * Add some logic
    
    * Make it namespace package
    
    * Add the dummy tests
    
    * Fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,17 @@
-import os
+from unittest import mock
 
+import pandas as pd
 import pytest
 
-import mlflow
 
+@pytest.fixture
+def mlflow_experiment():
+    with mock.patch("mlflow.search_runs") as mock_search_runs:
+        mock_data = {
+            "run_id": ["run_1", "run_2", "run_3"],
+            "tags.mlflow.runName": ["model_a", "model_b", "model_a"],
+        }
+        mock_df = pd.DataFrame(mock_data)
 
-@pytest.fixture(scope="function")
-def mlflow_experiment(tmp_path):
-    os.environ["MLFLOW_TRACKING_URI"] = f"file://{tmp_path}"
-    mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
-    experiment_id = mlflow.create_experiment("test-experiment")
-
-    with mlflow.start_run(run_name="test-run-1"):
-        mlflow.log_param("param1", 10)
-
-    with mlflow.start_run(run_name="test-run-2"):
-        mlflow.log_param("param2", 20)
-
-    # Simulate the multiple runs with the same name
-    with mlflow.start_run(run_name="test-run-2"):
-        mlflow.log_param("param2", 23)
-
-    yield experiment_id
-
-    # Cleanup
-    del os.environ["MLFLOW_TRACKING_URI"]
+        mock_search_runs.return_value = mock_df
+        yield "123"
